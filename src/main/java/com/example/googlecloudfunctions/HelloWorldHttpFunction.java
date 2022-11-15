3e05275a572876491cde206805db112c61fc7f09
mmarini <Matteo_Marini@bmc.com>
2022-11-15 18:12:46 +0100

    test

--- src/main/java/com/example/googlecloudfunctions/HelloWorldHttpFunction.java
+++ src/main/java/com/example/googlecloudfunctions/HelloWorldHttpFunction.java
@@ -9,11 +9,13 @@ import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
 
 @ApplicationScoped
+@Named("test")
 public class HelloWorldHttpFunction implements HttpFunction {
 
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
         Writer writer = httpResponse.getWriter();
         writer.write("Hello World");
+        System.out.println("Receive event Id: ");
     }
 }
