5dbb61b0bca48f3c197bb058feabaa7f230e1af1
mmarini <Matteo_Marini@bmc.com>
2022-11-19 18:10:05 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 gcloud functions deploy quarkus-example-http \
+  --zone=europe-west1-b \
   --entry-point=io.quarkus.gcp.functions.QuarkusHttpFunction \
   --runtime=java11 --trigger-http --allow-unauthenticated --source=target/deployment \
   --set-env-vars EXCHANGE_ADDRESS="192.168.1.3"
