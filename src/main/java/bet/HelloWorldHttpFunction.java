0bf386fcb2edc857efacea45d5d85199e8af7aa9
mmarini <Matteo_Marini@bmc.com>
2022-11-19 16:56:22 +0100

    test

--- src/main/java/bet/HelloWorldHttpFunction.java
+++ src/main/java/bet/HelloWorldHttpFunction.java
@@ -3,8 +3,10 @@ package bet;
 import java.io.Writer;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import bet.domain.Discover;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
@@ -12,6 +14,9 @@ import com.google.cloud.functions.HttpResponse;
 @ApplicationScoped
 @Named("test")
 public class HelloWorldHttpFunction implements HttpFunction {
+
+    @Inject
+    private Discover discover;
 
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
