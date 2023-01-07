4245526342abe8fcb8302aea23afb4006c71b219
mmarini <Matteo_Marini@bmc.com>
2023-01-07 02:02:57 +0100

    introduce pubsub

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ gcloud functions deploy quarkus-example-http \
   --region=europe-west1 \
   --entry-point=io.quarkus.gcp.functions.QuarkusHttpFunction \
   --runtime=java11 --trigger-http --allow-unauthenticated --source=target/deployment \
-  --set-env-vars EXCHANGE_ADDRESS="34.140.130.183:3000"
+  --set-env-vars EXCHANGE_ADDRESS="34.154.201.134:3000"
