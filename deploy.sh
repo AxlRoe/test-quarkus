70557b9832483d7458a101ef234f209175d5da81
mmarini <Matteo_Marini@bmc.com>
2023-03-03 11:09:21 +0100

    introduce pubsub

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+GCP_EXCHANGE_ADDRESS=$(gcloud compute addresses describe helloweb-ip --region europe-west8 | grep address: | awk '{print $NF}')
+
 gcloud functions deploy quarkus-example-http \
   --region=europe-west1 \
   --entry-point=io.quarkus.gcp.functions.QuarkusHttpFunction \
   --runtime=java11 --trigger-http --allow-unauthenticated --source=target/deployment \
-  --set-env-vars EXCHANGE_ADDRESS="34.154.201.134"
+  --set-env-vars EXCHANGE_ADDRESS=$GCP_EXCHANGE_ADDRESS
