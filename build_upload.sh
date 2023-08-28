3ec31ef4e3944c6ebfac50358a1425f8b2167727
mmarini <Matteo_Marini@bmc.com>
2023-08-28 18:54:14 +0200

    use terraform

--- build_upload.sh
+++ build_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./mvnw package
-gsutil cp target/deployment/test-quarkus-1.0-SNAPSHOT-runner.jar gs://dump-bucket-4/function
+gsutil cp target/deployment/test-quarkus-1.0-SNAPSHOT-runner.jar gs://dump-bucket-4/functions/
 echo "uploaded "
