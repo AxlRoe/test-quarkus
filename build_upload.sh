5ce1085c5eb58701fb71087becd567ea7a22de54
matteo <matteo.marini98@gmail.com>
2023-08-29 13:26:36 +0200

    test

--- build_upload.sh
+++ build_upload.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-zip source.zip *
-gsutil cp source.zip gs://dump-bucket-4/functions/
+./mvnw package
+cd ..
+cp -r test-quarkus source
+rm -rf source/.git
+cd source
+zip -r /tmp/source.zip .
+gsutil cp /tmp/source.zip gs://dump-bucket-4/functions/
+cd ..
+rm -rf source
+cd test-quarkus
