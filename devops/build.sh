f62b9c7c4c7fe6aa36a0918678079472df560393
mmarini <Matteo_Marini@bmc.com>
2023-09-06 02:23:19 +0200

    use terraform

--- devops/build.sh
+++ devops/build.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-export GOOGLE_APPLICATION_CREDENTIALS="/home/io/gcloud_home/gcp-dataflow-test/pubsub-to-bigq/data-flow-sa.json"
-
 echo "build "
 cd ..
 ./mvnw package
-
-gcloud functions deploy quarkus-example-http \
-  --gen2 \
-  --memory="256Mi"  \
-  --max-instances=2 \
-  --region $_REGION \
-  --vpc-connector $_VPC_CONNECTOR \
-  --entry-point=io.quarkus.gcp.functions.QuarkusHttpFunction \
-  --runtime=java11 --trigger-http --allow-unauthenticated --source=target/deployment
